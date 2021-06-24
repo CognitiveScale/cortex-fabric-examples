b63a272e6e8f40fb63c2fc1dc5389f030b7c1f7f
svangapally <svangapally@cognitivescale.com>
2021-06-24 15:50:38 +0530

    add version properties & copyrights

--- spark-batch-prediction/src/main/python/main.py
+++ spark-batch-prediction/src/main/python/main.py
@@ -1,3 +1,8 @@
+"""
+Copyright (c) 2021. Cognitive Scale Inc. All rights reserved.
+
+Licensed under CognitiveScale Example Code [License](https://cognitivescale.github.io/cortex-fabric-examples/LICENSE.md)
+"""
 import sys
 from cortex import Cortex
 from cortex.run import Run
@@ -8,36 +13,25 @@ import pandas as pd
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col
 
 
 def predict_partition(rows):
-    """ Calls a vectorized prediction by loading the partition into memory.
+    """ Predictions by loading the partition into memory
 
-        Parameters
-        ----------
-        rows : List[pyspark.sql.Row]
-            The rows of the partition.
-
-        Returns
-        -------
-        List[pyspark.sql.Row]
-            The predictions.
+    :param rows:  List[pyspark.sql.Row]
+    :return: [pyspark.sql.Row]
     """
     # Load the input rows into a data frame.
-    # It's safer to rely on the Row object's key-based
-    # lookup.
     rows_df = pd.DataFrame.from_records(
-        # For each row, convert to a dict mapping the column name to the features.
         [row.asDict() for row in rows]
     )
-    # It's possible the partition could be empty as an edge case.
     if rows_df.empty:
         return []
     model_artifact = broadcast_ml_model.value
 
-    # If the model artifact is of type __dict__
+    # If the model artifact is of type <dict>
     if isinstance(model_artifact, dict):
-        rows_df.columns = model_artifact["columns"]
         cat_cols = model_artifact["cat_columns"] if "cat_columns" in model_artifact else []
         num_cols = [x for x in rows_df.columns if x not in cat_cols]
         # Transforming the input data-frame using encoder & normalizer from the experiment artifact
@@ -54,12 +48,15 @@ def predict_partition(rows):
         df = pd.DataFrame(x_transform)
         rows_df.loc[:, 'prediction'] = broadcast_ml_model.value["model"].predict(df.values)
     else:
-        # If the model object is the trained model itself
+        # If the model object is an instance of model itself
         rows_df.loc[:, 'prediction'] = broadcast_ml_model.value.predict(rows_df.values)
-    rows_df = rows_df.apply(lambda x: x.astype(str) if x.dtype == 'float64' else x)
 
-    # Now we need to turn the predictions (a numpy array) into Rows again.
-    # This form assigns each column (plus "prediction") as a keyword argument.
+    data_dict = rows_df.dtypes.to_dict()
+    for key in data_dict.keys():
+        if data_dict[key].name == "float64":
+            rows_df[key] = rows_df[key].apply(lambda x: (x,))
+
+    # Transforming predictions into Rows again
     make_row = lambda row: Row(**{col: row[1][col] for col in rows_df.columns})
     return map(make_row, rows_df.iterrows())
 
@@ -81,7 +78,6 @@ def score_predictions(df, model, outcome, sc):
     # Broadcasting ML model across nodes for parallel prediction
     broadcast_ml_model = sc.broadcast(model)
     logging.info("Model Object:", broadcast_ml_model.value)
-
     # Scoring using the Model
     return df.rdd.mapPartitions(predict_partition).toDF().withColumnRenamed("prediction", outcome)
 
@@ -127,6 +123,10 @@ def make_batch_predictions(input_params):
 
         # Make predictions
         df = score_predictions(df, model, outcome, sc)
+        # Converting struct to double
+        for t in df.dtypes:
+            if t[1] == "struct<_1:double>":
+                df = df.withColumn(t[0], col(t[0]).getField("_1"))
 
         # Writing to output
         df.write.csv(output_path, mode='append', header=True)
@@ -145,8 +145,11 @@ def make_batch_predictions(input_params):
         df = df.drop(outcome, "_id")
         logging.info(df.printSchema())
 
-        # Make predictions
+        # Score predictions
         df = score_predictions(df, model, outcome, sc)
+        for t in df.dtypes:
+            if t[1] == "struct<_1:double>":
+                df = df.withColumn(t[0], col(t[0]).getField("_1"))
 
         # Writing to output
         df.write.format("com.mongodb.spark.sql.DefaultSource") \
