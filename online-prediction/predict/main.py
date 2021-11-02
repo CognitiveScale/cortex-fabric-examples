c5081bc63766da9ead20a0dbadbbc59c05d5cc9e
svangapally-cs <43963447+svangapally-cs@users.noreply.github.com>
2021-11-02 16:34:28 +0530

    Fab 2781: Online Prediction: Make artifact key configurable when loading model from experiment. (#50)
    * Added artifact-key to skill properties
    
    * renamed artifact key property
    
    * updated ReadME
    
    * Modified types.yaml

--- online-prediction/predict/main.py
+++ online-prediction/predict/main.py
@@ -1,6 +1,5 @@
 """
 Copyright (c) 2021. Cognitive Scale Inc. All rights reserved.
-
 Licensed under CognitiveScale Example Code [License](https://cognitivescale.github.io/cortex-fabric-examples/LICENSE.md)
 """
 import logging
@@ -27,7 +26,7 @@ def init(request: InitializeRequest):
 
     try:
         load_model(request.api_endpoint, request.token, request.project_id, request.properties.experiment_name,
-                   request.properties.run_id)
+                   request.properties.run_id, request.properties.artifact_key)
     except ValueError as e:
         logging.error(traceback.format_exc())
         raise HTTPException(status_code=400, detail=str(e))
