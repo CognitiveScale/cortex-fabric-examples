b45683eb1f957aa3ccc1c93df4be37c2fb144c7e
Jay Lara <jlara@cognitivescale.com>
2021-11-11 09:55:45 -0600

    created word-count-daemon using vscode plugin

--- word-count-agent/deploy.sh
+++ word-count-agent/deploy.sh
@@ -7,11 +7,11 @@ IMAGE_TAG=test1
 DEBUG=* cortex docker login
 
 # BUILD, PUSH, DEPLOY, AND SAVE word-count-daemon
-docker build -t ${DOCKER_REGISTRY}/word-count-daemon:${IMAGE_TAG} -f "${SCRIPT_DIR}"/skills/word-count-daemon/Dockerfile "${SCRIPT_DIR}"/skills/word-count-daemon
+docker build -t ${DOCKER_REGISTRY}/word-count-daemon:${IMAGE_TAG} -f "${SCRIPT_DIR}"/skills/word-count-daemon/actions/word-count-daemon/Dockerfile "${SCRIPT_DIR}"/skills/word-count-daemon/actions/word-count-daemon
 docker push ${DOCKER_REGISTRY}/word-count-daemon:${IMAGE_TAG}
 
 cortex actions deploy --actionName 'word-count-daemon' --actionType 'daemon' --port 5000 --docker ${DOCKER_REGISTRY}/word-count-daemon:${IMAGE_TAG}
-cortex skills save "${SCRIPT_DIR}"/skills/word-count-daemon/skill.yaml -y
+cortex skills save "${SCRIPT_DIR}"/skills/word-count-daemon/actions/word-count-daemon/skill.yaml -y
 
 
 # Deploy agent
