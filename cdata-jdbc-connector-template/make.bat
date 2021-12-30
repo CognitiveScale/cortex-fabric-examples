776b3b038d347ec3d414fecc0f0a6262b2747181
ljha <ljha@cognitivescale.com>
2021-12-30 21:36:56 +0530

    script for deployment on windows

---
+++ cdata-jdbc-connector-template/make.bat
@@ -0,0 +1,51 @@
+@echo off
+
+SET SKILL_NAME=cdata-jdbc-connector
+SET IMAGE_TAG=latest
+SET DOCKER_IMAGE_URL=%DOCKER_PREGISTRY_URL%/%SKILL_NAME%:%IMAGE_TAG%
+
+GOTO :checkenv %*
+
+:build
+	echo Building %SKILL_NAME% action image...
+    cmd /c docker build -t %DOCKER_IMAGE_URL% .
+	echo done building %SKILL_NAME% action image
+	GOTO :EOF
+
+:push
+	echo Pushing %SKILL_NAME% action image ...
+    :: cmd /c cortex docker login
+    cmd /c docker push %DOCKER_IMAGE_URL%
+	echo done pushing %SKILL_NAME% action image
+	GOTO :EOF
+
+:deploy
+	echo Deploying %SKILL_NAME% ...
+    cmd /c cortex actions deploy --actionName cdata-jdbc-connector --actionType daemon --cmd '["/bin/sh","-c","java -jar /app/server.jar"]' --project %PROJECT_NAME% --docker %DOCKER_IMAGE_URL%
+    cmd /c cortex actions deploy --actionName grc-jdbc-connector --actionType daemon --cmd '["/bin/sh","-c","java -jar /app/server.jar"]' --project %PROJECT_NAME% --docker %DOCKER_IMAGE_URL%
+    cmd /c cortex skills save -y skill.yaml --project %PROJECT_NAME%
+	echo Waiting for Skill %SKILL_NAME% to deploy
+	TIMEOUT 10
+	echo Deployed %SKILL_NAME%. Check "actionStatuses" for deployment status
+	GOTO :get
+
+:tests
+	echo Testing %SKILL_NAME%
+    cmd /c cortex skills invoke --params "{}" %SKILL_NAME% request --project %PROJECT_NAME%
+	GOTO :EOF
+
+:get
+    cmd /c cortex skills describe %SKILL_NAME%  --verbose --project %PROJECT_NAME%
+	GOTO :EOF
+
+:checkenv
+	echo Validating environment variables
+	IF "!DOCKER_PREGISTRY_URL!" == "" (
+		echo Eenvironment variable DOCKER_PREGISTRY_URL is not set. Set it like <docker-registry-url>/<namespace-org>"
+		exit/b 1
+	)
+	IF "!PROJECT_NAME!" == ""  (
+		echo  "Environment variable PROJECT_NAME is not set. Set this to Cortex project name."
+		exit/b 1
+	)
+	GOTO :%1
