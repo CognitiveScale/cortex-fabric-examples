37363a8cdb442792c511b9c7282967510d596970
Bikash Pandey <bpandey@cognitivescale.com>
2022-05-24 03:08:08 +0530

    skaffolding version that works

---
+++ spark-stream-ingest/spark-base/build-spark-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+spark_version=3.2.0
+hadoop_version=3.2
+
+cd spark-base
+
+curl https://archive.apache.org/dist/spark/spark-${spark_version}/spark-${spark_version}-bin-hadoop${hadoop_version}.tgz -o spark.tgz
+
+tar -xf spark.tgz && cd spark-${spark_version}-bin-hadoop${hadoop_version}
+
+docker build -t spark-base -f kubernetes/dockerfiles/spark/Dockerfile .
+
+docker build --build-arg base_img=spark-base:latest -t spark-base-python -f kubernetes/dockerfiles/spark/bindings/python/Dockerfile .
